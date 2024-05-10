2e3cb0aa838e9b4a8108130a90ab4fb3c3ab7d1a
ThanhPhong203 <lethanhphong05092003@gmail.com>
2024-05-10 15:34:53 +0700

    Assignment

---
+++ src/main/java/Exercise2Assign2.java
@@ -0,0 +1,11 @@
+
+public class Exercise2Assign2 {
+    public static void main(String[] args) {
+        // Question 1
+        int a = 1;
+        System.out.println("So nguyen la : " + a);
+        // Question 2
+        System.out.printf(Locale.ITALY, "Money = %,d%n", 1234560000);
+    }
+
+}

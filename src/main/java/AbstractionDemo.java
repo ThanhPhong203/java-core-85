52f32f29a6088169c6e6e9b6a6884dd7f837344b
ThanhPhong203 <lethanhphong05092003@gmail.com>
2024-05-15 21:55:51 +0700

    lesson-06

---
+++ src/main/java/AbstractionDemo.java
@@ -0,0 +1,14 @@
+public class AbstractionDemo {
+    public static void main(String[] args) {
+        // tinh truu tuong abstract class .interface
+        // 1. khong the khoi tao doi tuong abstract class
+        // Animal animal = new Animal()
+
+        // 2. class con phai la abstract class
+        // hoac phai trien khai abstract method cua class cha
+        Cat cat = new Cat();
+        cat.makeSound();
+
+        // 3.Abstract class co the khong co abstract method nao
+    }
+}

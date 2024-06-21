7c1c2a427c4083505e1e5f25225b02bdc529421a
ThanhPhong203 <lethanhphong05092003@gmail.com>
2024-06-21 14:16:56 +0700

    finaexam1

--- src/main/java/com/vti/frontend/UserProgram.java
+++ src/main/java/com/vti/frontend/UserProgram.java
@@ -1,16 +1,26 @@
 package com.vti.frontend;
 
+import com.vti.controller.UserController;
+import com.vti.repository.IUserRepository;
 import com.vti.repository.UserRepository;
+import com.vti.service.IUserService;
+import com.vti.service.UserService;
 import com.vti.util.JdbcUtil;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class UserProgram {
-    public static void main(String[] args) throws SQLException {
-        UserRepository repository= new UserRepository();
-        UserFunction function= new UserFunction(repository);
-        function.findAll();
-        function.findById();
+    public static void main(String[] args) {
+        IUserRepository repository= new UserRepository();
+        IUserService service = new UserService(repository);
+        UserController controller = new UserController(service);
+        UserFunction function= new UserFunction(controller);
+        function.showMenu();
+
+
+
+
 
     }
 
--- src/main/java/com/vti/repository/UserRepository.java
+++ src/main/java/com/vti/repository/UserRepository.java
@@ -3,12 +3,14 @@ package com.vti.repository;
 import com.vti.entity.User;
 import com.vti.util.JdbcUtil;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
-public class UserRepository {
-    public List<User> findAll() throws SQLException {
+public class UserRepository implements IUserRepository {
+    @Override
+    public List<User> findAll() throws SQLException, IOException {
         String sql = "SELECT * FROM users ";
         try (
                 Connection connection = JdbcUtil.getConnection();
@@ -34,7 +36,8 @@ public class UserRepository {
         }
 
     }
-    public User findById(int id) throws SQLException {
+    @Override
+    public User findById(int id) throws SQLException, IOException {
         String sql = "SELECT * FROM users WHERE id = ? ";
         try (
                 Connection connection = JdbcUtil.getConnection();
@@ -61,6 +64,55 @@ public class UserRepository {
 
         }
 
+    }
+    @Override
+    public User findByEmailAndPassword(String email, String password) throws SQLException, IOException {
+        String sql = "{CALL find_by_email_and_password(?, ?)}";
+        try (
+                Connection connection = JdbcUtil.getConnection();
+                CallableStatement cStmt = connection.prepareCall(sql)
+        ) {
+            cStmt.setString(1, email);
+            cStmt.setString(2, password);
+            try (ResultSet rs = cStmt.executeQuery()) {
+                if (rs.next()) {
+                    User user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setFullName(rs.getString("full_name"));
+                    user.setEmail(rs.getString("email"));
+                    user.setPassword(rs.getString("password"));
+                    String role = rs.getString("role");
+                    user.setRole(User.Role.valueOf(role));
+                    user.setProSkill(rs.getString("pro_skill"));
+                    user.setExpInYear(rs.getInt("exp_in_year"));
+                    return user;
+                }
+                return null;
+            }
+        }
+    }
+    @Override
+    public int create(String fullName, String email) throws SQLException, IOException {
+        String sql = "INSERT INTO users (full_name, email) VALUES (?, ?)";
+        try (
+                Connection connection = JdbcUtil.getConnection();
+                PreparedStatement pStmt = connection.prepareStatement(sql)
+        ) {
+            pStmt.setString(1, fullName);
+            pStmt.setString(2, email);
+            return pStmt.executeUpdate();
+        }
+    }
+    @Override
+    public int deleteById(int id) throws SQLException, IOException {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (
+                Connection connection = JdbcUtil.getConnection();
+                PreparedStatement pStmt = connection.prepareStatement(sql)
+        ) {
+            pStmt.setInt(1, id);
+            return pStmt.executeUpdate();
+        }
     }
 
 }
